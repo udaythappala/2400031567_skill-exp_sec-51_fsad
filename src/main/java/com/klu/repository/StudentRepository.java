a5f8b0a1f644bc364222e94f6aa8b1a2471bacc7
THAPPALA UDAY <udaythappala@gmail.com>
2026-03-27 11:39:17 +0530

    Delete src directory

--- src/main/java/com/klu/repository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.klu.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.klu.entity.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Integer>{
-
-}
